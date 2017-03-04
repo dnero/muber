e42abc07e13acb227cf83c25da71acafc9d69f30
D'ontreye Nero <dontreyenero@gmail.com>
2017-03-04 16:30:54 -0500

    Add error-handling middleware

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,11 +1,12 @@
 const Driver = require('../models/driver');
 
 module.exports = {
-	create(req, res) {
+	create(req, res, next) {
 		const driverProps = req.body;
 
 		Driver.create(driverProps)
-			.then(driver => res.send(driver));
+			.then(driver => res.send(driver))
+			.catch(next);
 	},
 
 	greeting(req, res) {
